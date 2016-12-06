da4ae1c45037b4de722b3a81028d7d1dd0905424
DESKTOP-8RMKUQ8\11710 <1171067930@qq.com>
2016-12-06 22:56:49 +0800

    列出创建数据库

--- routes/db_connect_string.js
+++ routes/db_connect_string.js
@@ -7,13 +7,20 @@ MongoClient.connect('mongodb://localhost:27017/Databases', function(err, db) {
     throw err;
   }else{
   	console.log("succeed");
+    //列出数据库
+    var adminDB = db.admin();
+    adminDB.listDatabases(function(err,databases){
+      console.log("Before Add Database List:");
+      console.log(databases);
+    });
+    //创建数据库
+    var newDB = db.db("newDB");
+    newDB.createCollection("newCollection",function(err,collection){
+      if(!err){
+        console.log("New Database Created");
+      }
+    });
   }
-  // db.collection('mammals').find().toArray(function(err, result) {
-  //   if (err) {
-  //     throw err;
-  //   }
-  //   console.log(result);
-  // });
 });
 
 
